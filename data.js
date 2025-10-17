4d24237d800ee209c85d21150e47d7d39873a22b
mort13 <mo@redback.de>
2025-10-17 19:47:40 +0200

    some building

--- data.js
+++ data.js
@@ -8,7 +8,9 @@ let miningData = {
 async function loadMiningData() {
   const endpoints = ["laserheads", "modules", "gadgets"];
   for (const ep of endpoints) {
-    const res = await fetch(`https://uexcorp.space/api/v2/mining/${ep}`);
+    const res = await fetch('https://uexcorp.space/api/v2/mining/laserheads', {
+  headers: { 'Authorization': '96e0d7370dc04112d89722be37babd9f61dd4bfa' }
+})
     const json = await res.json();
     miningData[ep] = json;
   }
